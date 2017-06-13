3e214c07d31b094bb2ef25b68235cd356d18da03
Niccolo99 <tortelli.niccolo@gmail.com>
2017-06-13 10:59:47 +0200

    Aggiunta dei commenti

--- main-cyclic.py
+++ main-cyclic.py
@@ -1,8 +1,6 @@
 import datetime
 import shutil
-
 a = 0
-
 from mount import *
 
 
@@ -19,33 +17,32 @@ def get_partition(device):
     return return_partitions[0]
 
 
-file_path = "/media/sdb"
+file_path = "/media/sdb"  # definisce il path della chiavetta
 
 
-def mount(device, name=None):
+def mount(device, name=None):  # definisce la funzione mount
     if not name:
         name = get_device_name(device)
     mount_partition(get_partition(device), name)
 
 
 devices = list_media_devices()
-if devices:
+if devices:  # controlla se e' presente la chiavetta
     device = devices[0]
     path = get_media_path(device)
     mount(device)
 
-    if is_mounted(device):
+    if is_mounted(device):  # controlla se e' montata la chiavetta
         print device, "\nsalva sulla chiavetta"
         datetime.datetime.now()
         print datetime.datetime.now()
         shutil.copy2('/home/stage/cartellafatta/localfile.txt', path)  # copia il file da dispositivo a usb
         if path + 'localfile.txt':
 
-            os.remove('/home/stage/cartellafatta/localfile.txt')
-            os.rmdir('/home/stage/cartellafatta/')
+            os.remove('/home/stage/cartellafatta/localfile.txt')  # cancella il file da locale
+            os.rmdir('/home/stage/cartellafatta/')  # cancella la cartella che conteneva il file in locale
 
-            # with (file_path + "/file-usb.txt", "w") as f:  # e' il file principale
-            a = 0
+            a = 0  # ciclo di cyclic che scrive in usb un file
             while a <= 100:
                 a += 1
                 text = "riga nuova" + str(a) + "\n"
@@ -71,7 +68,7 @@ if devices:
 
 
 
-else:
+else:  # se non e' presente la chiavetta crea una cartella con file in locale
 
     print("salva in locale")
     datetime.datetime.now()
@@ -79,9 +76,7 @@ else:
     path = "/home/stage/cartellafatta/"
     os.system("mkdir -p " + path)
 
-    # with open(path + "localfile.txt", "w") as fl:  # file temporaneo
-
-    a = 0
+    a = 0  # ciclo che scrive nel file in locale
 
     while a <= 100:
         a += 1
